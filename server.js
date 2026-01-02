9c387676893a68acebcee118ed9a7e0328113c5f
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2026-01-02 16:54:01 +0530

    feat:- change  some url

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const http = require("node:http");
 const server = http.createServer(function (req, res) {
+    /** If you want change some url  */
+  if (req.url === "/getSecretData") {
+    res.end("There is no secret Data");
+  }
   res.end("Hello - Ashish");
 });
 
