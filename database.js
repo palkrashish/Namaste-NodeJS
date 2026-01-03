c383ea1c20d95a7d12d759da8e06c1edb19cb13b
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2026-01-04 00:53:44 +0530

    insert some document

--- database.js
+++ database.js
@@ -13,12 +13,38 @@ async function main() {
   console.log("Connected successfully to server");
   const db = client.db(dbName);
   const collection = db.collection("User");
-  
 
+  // Insert document
+  const insertResult = await collection.insertMany([
+    {
+      a: 1,
+      firstName: "Rahul",
+      LastName: "Pal",
+      City: "Noida",
+      Country: "India",
+    },
+    {
+      a: 2,
+      firstName: "Mohan",
+      LastName: "Raj",
+      City: "Noida",
+      Country: "India",
+    },
+    {
+      a: 3,
+      firstName: "Sohan",
+      LastName: "Rastogi",
+      City: "Noida",
+      Country: "India",
+    },
+  ]);
+  console.log("Inserted documents =>", insertResult);
+  // Find All document
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
   return "done.";
 }
-  main()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
-
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
